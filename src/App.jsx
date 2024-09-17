b13ede7bc238a264be137354eeaa4e60d6ef7375
igaddy79 <igaddy79@yahoo.com>
2024-09-16 23:00:14 -0400

    initial commit remove div syntax

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      
+      <div className="card">
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
